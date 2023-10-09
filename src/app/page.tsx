d3c6b407dbc9a4f1176e6851fcf35de8263100f0
Sammish <samd93@hotmail.co.uk>
2023-10-09 18:47:55 +0200

    added home screen text to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <div>Kode her</div>
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <a href="./products" className="text-center text-xl hover:text-blue-600">
+        To the /products page
+      </a>
+      <p>
+        I wasn't able to get the tests running with typescript and my
+        refactoring is a bit of a mess (ended up testing out tailwind half way
+        through).
+      </p>
+    </div>
+  )
 }
